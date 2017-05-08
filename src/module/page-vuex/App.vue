c740883242bc4286fb4fdc4f34e26cb90cd637bc
喵叔catuncle <andych008@163.com>
2017-05-08 13:59:57 +0800

    vuex mapState

--- src/module/page-vuex/App.vue
+++ src/module/page-vuex/App.vue
@@ -2,11 +2,14 @@
   <div>
     <Card style="width:320px">
       <p slot="title" style="text-align:center">标准卡片</p>
+      <p>{{this.$store.state.count}}</p>
       <p>{{value}}</p>
+      <p>{{count}}</p>
     </Card>
-
-    <Button @click="increment">加1</Button>
-    <Button @click="decrement">减1</Button>
+    <Button-group>
+      <Button @click="increment">加1</Button>
+      <Button @click="decrement">减1</Button>
+    </Button-group>
   </div>
 
 </template>
@@ -15,6 +18,7 @@
   import Vue from 'vue'
   import iView from 'iview'
   import 'iview/dist/styles/iview.css';
+  import { mapState} from 'vuex'
 
   Vue.use(iView);
 
@@ -22,7 +26,8 @@
     computed: {
       value () {
         return this.$store.state.count
-      }
+      },
+        ...mapState(['count'])
     },
     methods: {
       increment () {
